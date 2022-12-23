1aa9ec41dddc44e960489b0ec86b74fd7ef1c3e3
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2022-12-23 18:52:07 +0100

    adding key to zoom

--- template/src/Mouse.cpp
+++ template/src/Mouse.cpp
@@ -11,34 +11,35 @@ int Mouse::lastX = 0;
 int Mouse::lastY = 0;
 int Mouse::lastZoom = 0;
 
+// Co-auteur pour cette fonction mouse(): Yahnis Saint-Val
 void mouse(int button, int state, int x, int y) {
-	if (state == GLUT_UP) {
-		Mouse::mouseMovePressed = false;
-		Mouse::mouseRotatePressed = false;
-		Mouse::mouseZoomPressed = false;
-	} else {
-		if (button == GLUT_LEFT_BUTTON) {
-			Context::camera.beginRotate(x, y);
-			Mouse::mouseMovePressed = false;
-			Mouse::mouseRotatePressed = true;
-			Mouse::mouseZoomPressed = false;
-			Context::refreshMatrices = true;
-		} else if (button == GLUT_RIGHT_BUTTON) {
-			Mouse::lastX = x;
-			Mouse::lastY = y;
-			Mouse::mouseMovePressed = true;
-			Mouse::mouseRotatePressed = false;
-			Mouse::mouseZoomPressed = false;
-		} else if (button == GLUT_MIDDLE_BUTTON) {
-			if (Mouse::mouseZoomPressed == false) {
-				Mouse::lastZoom = y;
-				Mouse::mouseMovePressed = false;
-				Mouse::mouseRotatePressed = false;
-				Mouse::mouseZoomPressed = true;
-			}
-		}
-	}
-	idle();
+    if (state == GLUT_UP) {
+        Mouse::mouseMovePressed = false;
+        Mouse::mouseRotatePressed = false;
+        Mouse::mouseZoomPressed = false;
+    } else {
+        /*if (button == GLUT_LEFT_BUTTON) {
+            Context::camera.beginRotate(x, y);
+            Mouse::mouseMovePressed = false;
+            Mouse::mouseRotatePressed = true;
+            Mouse::mouseZoomPressed = false;
+            Context::refreshMatrices = true;
+        } else*/ if (button == GLUT_RIGHT_BUTTON) {
+            Mouse::lastX = x;
+            Mouse::lastY = y;
+            Mouse::mouseMovePressed = true;
+            Mouse::mouseRotatePressed = false;
+            Mouse::mouseZoomPressed = false;
+        } else if (button == GLUT_MIDDLE_BUTTON || button == GLUT_LEFT_BUTTON) {
+            if (Mouse::mouseZoomPressed == false) {
+                Mouse::lastZoom = y;
+                Mouse::mouseMovePressed = false;
+                Mouse::mouseRotatePressed = false;
+                Mouse::mouseZoomPressed = true;
+            }
+        }
+    }
+    idle();
 }
 
 void motion(int x, int y) {
